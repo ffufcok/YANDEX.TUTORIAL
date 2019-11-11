a08b1009741a2cf7106872d317ce2a635b1f65ce
tol063115 <tol063115@mail.ru>
2019-11-11 15:27:32 +0300

    John, tol063115@mail.ru

--- Git и желтые окружности.py
+++ Git и желтые окружности.py
@@ -1,31 +1,37 @@
 import sys
-from PyQt5.QtWidgets import QWidget, QApplication
-from PyQt5.QtGui import QPainter, QColor
+from PyQt5 import uic
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
+from PyQt5.QtGui import QPainter, QBrush
+from PyQt5.QtCore import Qt
+import random
 
 
-class Example(QWidget):
+class Example(QMainWindow):
     def __init__(self):
         super().__init__()
+        uic.loadUi('Git и желтые окружности.ui', self)
+        self.flag = False
         self.initUI()
 
     def initUI(self):
-        self.setGeometry(300, 300, 200, 200)
         self.setWindowTitle('Git и желтые окружности')
+        self.pushButton.clicked.connect(self.drawf)
         self.show()
 
     def paintEvent(self, event):
-        qp = QPainter()
-        qp.begin(self)
-        self.drawFlag(qp)
-        qp.end()
+        self.painter = QPainter(self)
+        self.painter.setBrush(QBrush(Qt.yellow, Qt.SolidPattern))
+        a = random.randint(40, 60)
+        self.painter.drawEllipse(random.randint(0, 500), random.randint(0, 500), a,
+                                 a)
+        self.painter.end()
 
-    def drawFlag(self,qp):
-        qp.setBrush(QColor(255, 0, 0))
-        qp.drawRect(30, 30, 120, 30)
-        qp.setBrush(QColor(0, 255, 0))
-        qp.drawRect(30, 60, 120, 30)
-        qp.setBrush(QColor(0, 0, 255))
-        qp.drawRect(30, 90, 120, 30)
+    def drawf(self):
+        self.painter.setBrush(QBrush(Qt.yellow, Qt.SolidPattern))
+        a = random.randint(40, 60)
+        self.painter.drawEllipse(random.randint(0, 500), random.randint(0, 500), a,
+                                 a)
+        self.update()
 
 
 if __name__ == '__main__':
